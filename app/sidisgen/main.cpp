ef3bbda5c9ef47bbdfea0c3a7faf438f5fdd7627
Duane Byer <duane.byer@gmail.com>
2020-08-06 20:44:55 -0400

    Update sidisgen so that it can compute Born cross-sections with arbitrary polarizations

--- app/sidisgen/main.cpp
+++ app/sidisgen/main.cpp
@@ -1,6 +1,7 @@
 #include <cmath>
 #include <iostream>
 
+#include <algorithm>
 #include <sidis/sidis.hpp>
 #include <sidis/sf_model/ww.hpp>
 
@@ -14,28 +15,39 @@ int main(int argc, char** argv) {
 	Real mh = constant::MASS_PI;
 	Real M_th = constant::MASS_P + constant::MASS_PI;
 	Real pi = constant::PI;
-	kin::Initial initial {
+	kin::Initial initial_state {
 		math::Vec4(M, 0., 0., 0.),
 		math::Vec4(E_b, std::sqrt(E_b * E_b - m * m) * math::Vec3::Z),
 	};
-
 	// Choose the region of phase space to look at.
 	kin::PhaseSpace phase_space {
 		0.2,
 		0.9,
 		0.2,
-		pi / 6.,
+		2.,
 		5. * pi / 6.,
 		-2. * pi / 6.
 	};
 	// Do the kinematic calculations.
-	kin::Kinematics kin(initial, phase_space, mh, M_th);
+	kin::Kinematics kin(initial_state, phase_space, mh, M_th);
+	// Get the final state particles.
+	kin::Final final_state(initial_state, kin);
+	// Choose beam and target polarizations.
+	Real beam_pol = 1.;
+	math::Vec3 target_pol(0.0, 0.0, 1.0);
+	// Transform the target polarization into a different basis.
+	math::Vec3 e_z = final_state.q.r.unit();
+	math::Vec3 e_y = math::cross(e_z, final_state.ph.r).unit();
+	math::Vec3 e_x = math::cross(e_y, e_z).unit();
+	math::Vec3 eta(
+		math::dot(target_pol, e_x),
+		math::dot(target_pol, e_y),
+		math::dot(target_pol, e_z));
 	// Choose structure functions. For now, use the WW approximation.
 	sf::model::WW ww;
 	sf::Sf sf = ww.sf(kin.x, kin.z, kin.Q_sq, kin.ph_t);
 	// Compute the cross-section.
-	Real born_cross_section = xs::born(0., math::Vec3::ZERO, kin, sf);
-
+	Real born_cross_section = xs::born(beam_pol, eta, kin, sf);
 	std::cout << "Born cross-section is: " << born_cross_section << std::endl;
 	return 0;
 }
