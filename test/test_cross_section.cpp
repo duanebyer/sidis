1cd6b8bd9c46505add39d0c3a0a044d5643541ea
Duane Byer <duane.byer@gmail.com>
2020-08-12 14:31:31 -0400

    Update cross-section tests

--- test/test_cross_section.cpp
+++ test/test_cross_section.cpp
@@ -1,5 +1,6 @@
 #include <catch2/catch.hpp>
 
+#include <sstream>
 #include <tuple>
 
 #include <sidis/sidis.hpp>
@@ -23,12 +24,13 @@ struct Output {
 	RelMatcher<Real> amm;
 	RelMatcher<Real> delta_vr;
 	RelMatcher<Real> delta_vac_lep;
+	RelMatcher<Real> delta_vac_had;
 };
 
 }
 
 TEST_CASE(
-		"Cross-section values",
+		"Non-radiative cross-section values",
 		"[xs]") {
 	// Load the structure function data ahead of time.
 	sf::model::WW ww;
@@ -38,16 +40,17 @@ TEST_CASE(
 	auto target = GENERATE(
 		Tuple {
 			{
-				272.3289320834889,
-				0.7742207157181373,
-				{ 0.0100723080218603, -0.0074201560094463, -0.0861055915456175 },
-				{ 0.2438003952188158, 0.1172111539548094, 0.7003402901345090, 0.9374573911481556, -0.5714773016683213, -2.179063668473446 },
+				244.3994717676956,
+				-0.6164827543415216,
+				{ -0.4557051628146152, 0.09688945772348936, -0.4280942449984859 },
+				{ 0.2195446560618665, 0.03029886835408146, 0.2387660175848276, 0.5575045307531898, -2.344259584082282, 0.1116601857460971 },
 			},
 			{
-				RelMatcher<Real>(4.95243870781e-7, 1e-11),
-				RelMatcher<Real>(9.6695598884e-18, 1e-10),
-				RelMatcher<Real>(-96.666414580, 1e-11),
-				RelMatcher<Real>(14.8883577396684, 1e-14),
+				RelMatcher<Real>(1.2860080823885e-3, 2.1e-13),
+				RelMatcher<Real>(1.97833608719e-14, 3.1e-12),
+				RelMatcher<Real>(-99.4643915906, 1.0e-12),
+				RelMatcher<Real>(12.4986683926813, 4.2e-15),
+				RelMatcher<Real>(5.07031143940847, 1.2e-15),
 			},
 		});
 	Input input = std::get<0>(target);
@@ -68,34 +71,42 @@ TEST_CASE(
 	kin::Kinematics kin(initial_state, phase_space, mh, M_th);
 	// Get the final state particles.
 	kin::Final final_state(initial_state, kin);
-	// Choose beam and target polarizations.
+	// Get beam and target polarizations.
 	Real beam_pol = input.beam_pol;
 	math::Vec3 target_pol = input.target_pol;
-	// Transform the target polarization into a different basis.
-	math::Vec3 e_z = final_state.q.r.unit();
-	math::Vec3 e_y = math::cross(e_z, final_state.ph.r).unit();
-	math::Vec3 e_x = math::cross(e_y, e_z).unit();
-	math::Vec3 eta(
-		math::dot(target_pol, e_x),
-		math::dot(target_pol, e_y),
-		math::dot(target_pol, e_z));
 	// Calculate structure functions.
 	sf::Sf sf = ww.sf(kin.x, kin.z, kin.Q_sq, kin.ph_t);
 	// Compute the cross-sections.
-	Real born = xs::born(beam_pol, eta, kin, sf);
-	Real amm = xs::amm(beam_pol, eta, kin, sf);
+	Real born = xs::born(beam_pol, target_pol, kin, sf);
+	Real amm = xs::amm(beam_pol, target_pol, kin, sf);
 	Real delta_vr = xs::delta_vr(kin);
 	Real delta_vac_lep = xs::delta_vac_lep(kin);
+	Real delta_vac_had = xs::delta_vac_had(kin);
+
+	// Print state information.
+	std::stringstream ss;
+	ss << "x     = " << std::to_string(phase_space.x)       << std::endl;
+	ss << "y     = " << std::to_string(phase_space.y)       << std::endl;
+	ss << "z     = " << std::to_string(phase_space.z)       << std::endl;
+	ss << "ph_t² = " << std::to_string(phase_space.ph_t_sq) << std::endl;
+	ss << "φ_h   = " << std::to_string(phase_space.phi_h)   << std::endl;
+	ss << "φ     = " << std::to_string(phase_space.phi)     << std::endl;
+	ss << "λ_e   = " << std::to_string(beam_pol)            << std::endl;
+	ss << "η_1   = " << std::to_string(target_pol.x)        << std::endl;
+	ss << "η_2   = " << std::to_string(target_pol.y)        << std::endl;
+	ss << "η_3   = " << std::to_string(target_pol.z);
+	INFO(ss.str());
 
 	// Do comparisons.
 	CHECK_THAT(born, output.born);
 	CHECK_THAT(amm, output.amm);
 	CHECK_THAT(delta_vr, output.delta_vr);
 	CHECK_THAT(delta_vac_lep, output.delta_vac_lep);
+	CHECK_THAT(delta_vac_had, output.delta_vac_had);
 }
 
 TEST_CASE(
-		"Cross-section values with muon",
+		"Non-radiative cross-section values with muon",
 		"[xs]") {
 	// Test in the regime where `m` is comparable to `M`.
 }
