544d25dec67cb9bfee29e8ac25567d8a6ed15920
Duane Byer <duane.byer@gmail.com>
2020-08-12 14:30:27 -0400

    Update constant values with values from PDG

--- include/sidis/constant.hpp
+++ include/sidis/constant.hpp
@@ -8,14 +8,15 @@ namespace constant {
 
 static Real const PI = 3.141592653589793238462643383279502797479068098137295573004504331874296718662975536062731407582759857177734375;
 
-static Real const ALPHA = 1./137.035999084;
+static Real const ALPHA = 7.2973525664e-3;
 
-static Real const MASS_P = 0.93827208816;
-static Real const MASS_E = 0.00051099895000;
-static Real const MASS_MU = 0.1056583755;
+static Real const MASS_P = 0.9382720813;
+
+static Real const MASS_E = 0.0005109989461;
+static Real const MASS_MU = 0.1056583745;
 static Real const MASS_TAU = 1.77686;
-static Real const MASS_PI = 0.13957018;
-static Real const MASS_PI_0 = 0.1349766;
+static Real const MASS_PI = 0.13957061;
+static Real const MASS_PI_0 = 0.1349770;
 
 }
 }
