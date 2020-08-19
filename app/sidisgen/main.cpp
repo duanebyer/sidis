b54c7bf7cff00ebf8a7d54e44e2920f89b75a1e0
Duane Byer <duane.byer@gmail.com>
2020-08-19 11:01:38 -0400

    Include radiative cross-section calculation in sidisgen for testing

--- app/sidisgen/main.cpp
+++ app/sidisgen/main.cpp
@@ -23,16 +23,19 @@ int main(int argc, char** argv) {
 		0.2,
 		2.,
 		5. * pi / 6.,
-		-2. * pi / 6.
+		-2. * pi / 6.,
 	};
 	// Do the kinematic calculations.
 	kin::Kinematics kin(initial_state, phase_space, mh, M_th);
+	kin::KinematicsRad kin_rad(kin, 0.2, 2. * pi / 6., 1.);
 	// Get the final state particles.
 	kin::Final final_state(initial_state, kin);
+	kin::FinalRad final_state_rad(initial_state, kin_rad);
 	// Choose beam and target polarizations.
 	Real beam_pol = 1.;
 	math::Vec3 target_pol(0.0, 0.0, 1.0);
-	// Transform the target polarization into a different basis.
+	// Transform the target polarization into a different basis (same for
+	// radiative and non-radiative parts).
 	math::Vec3 e_z = final_state.q.r.unit();
 	math::Vec3 e_y = math::cross(e_z, final_state.ph.r).unit();
 	math::Vec3 e_x = math::cross(e_y, e_z).unit();
@@ -43,11 +46,14 @@ int main(int argc, char** argv) {
 	// Choose structure functions. For now, use the WW approximation.
 	sf::model::WW ww;
 	sf::Sf sf = ww.sf(kin.x, kin.z, kin.Q_sq, kin.ph_t);
+	sf::Sf shift_sf = ww.sf(kin_rad.shift_x, kin_rad.shift_z, kin_rad.shift_Q_sq, kin_rad.shift_ph_t);
 	// Compute the cross-sections.
-	Real born_cross_section = xs::born(beam_pol, eta, kin, sf);
-	Real amm_cross_section = xs::amm(beam_pol, eta, kin, sf);
-	std::cout << "Born cross-section is: " << born_cross_section << std::endl;
-	std::cout << "AMM cross-section is:  " << amm_cross_section << std::endl;
+	Real born_xs = xs::born(beam_pol, eta, kin, sf);
+	Real amm_xs = xs::amm(beam_pol, eta, kin, sf);
+	Real rad_xs = xs::rad(beam_pol, eta, kin_rad, sf, shift_sf);
+	std::cout << "Born cross-section is:      " << born_xs << std::endl;
+	std::cout << "AMM cross-section is:       " << amm_xs << std::endl;
+	std::cout << "Radiative cross-section is: " << rad_xs << std::endl;
 	return 0;
 }
 
