96cdbd3720fc1671df0985f00eaf6158c578221f
Duane Byer <duane.byer@gmail.com>
2020-09-16 10:43:32 -0400

    Add Reimann integration method

--- include/sidis/integrate.hpp
+++ include/sidis/integrate.hpp
@@ -5,6 +5,17 @@ namespace sidis {
 namespace math {
 
 template<typename R, typename F>
+R reimann(F f, R a, R b, unsigned n) {
+	R delta = (b - a) / n;
+	R result = 0.;
+	for (unsigned i = 0; i < n; ++i) {
+		R x = (i * b + (n - i) * a + 0.5) / n;
+		result += f(x);
+	}
+	return result * delta;
+}
+
+template<typename R, typename F>
 R trapezoid(F f, R a, R b, unsigned n) {
 	R delta = (b - a) / n;
 	R result = (f(a) + f(b)) / 2;
