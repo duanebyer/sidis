ee0e72864cabbd093973593e9c580d4146027143
Duane Byer <duane.byer@gmail.com>
2020-09-16 10:44:21 -0400

    Modify trapezoidal integration for better numerical precision.

--- include/sidis/integrate.hpp
+++ include/sidis/integrate.hpp
@@ -18,9 +18,9 @@ R reimann(F f, R a, R b, unsigned n) {
 template<typename R, typename F>
 R trapezoid(F f, R a, R b, unsigned n) {
 	R delta = (b - a) / n;
-	R result = (f(a) + f(b)) / 2;
+	R result = 0.5 * (f(a) + f(b));
 	for (unsigned i = 1; i <= n - 1; ++i) {
-		R x = i * (b - a) / n + a;
+		R x = (i * b + (n - i) * a) / n;
 		result += f(x);
 	}
 	return result * delta;
