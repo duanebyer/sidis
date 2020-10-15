496bdbca3daaf9046882f857df294d18f048e2bb
Duane Byer <duane.byer@gmail.com>
2020-10-15 09:28:55 -0400

    Add ROOT dependency to the sidisgen app

--- app/sidisgen/main.cpp
+++ app/sidisgen/main.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
 
+#include <TCanvas.h>
+#include <TF1.h>
+
 #include <sidis/sidis.hpp>
 
 using namespace sidis;
 
 int main(int argc, char** argv) {
-	std::cout << "Hello world." << std::endl;
+	char const* func_def = "sin(x)/x";
+	std::cout << "Plotting a function " << func_def << std::endl << std::endl;
+	TF1 f1("f1", func_def, -10., 10.);
+	TCanvas c;
+	f1.Draw();
+	c.Print("f1.png");
 	return 0;
 }
 
