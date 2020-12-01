7e1fb85530ec761614bc344f089778801cd270df
Duane Byer <duane.byer@gmail.com>
2020-12-01 12:01:52 -0500

    Add several new constants

--- include/sidis/constant.hpp
+++ include/sidis/constant.hpp
@@ -8,21 +8,22 @@ namespace constant {
 
 static Real const PI = 3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679821480865132823066470938446L;
 static Real const E = 2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174136L;
+extern Real const INF;
 
-static Real const ALPHA = 7.2973525664e-3;
+static Real const ALPHA = 7.2973525664e-3L;
 
-static Real const MASS_E = 0.0005109989461;
-static Real const MASS_MU = 0.1056583745;
-static Real const MASS_TAU = 1.77686;
+static Real const MASS_E = 0.0005109989461L;
+static Real const MASS_MU = 0.1056583745L;
+static Real const MASS_TAU = 1.77686L;
 
-static Real const MASS_P = 0.9382720813;
-static Real const MASS_N = 0.939565413;
-static Real const MASS_D = 1.875612928;
+static Real const MASS_P = 0.9382720813L;
+static Real const MASS_N = 0.939565413L;
+static Real const MASS_D = 1.875612928L;
 
-static Real const MASS_PI_0 = 0.1349770;
-static Real const MASS_PI = 0.13957061;
-static Real const MASS_K_0 = 0.497611;
-static Real const MASS_K = 0.493677;
+static Real const MASS_PI_0 = 0.1349770L;
+static Real const MASS_PI = 0.13957061L;
+static Real const MASS_K_0 = 0.497611L;
+static Real const MASS_K = 0.493677L;
 
 enum class Lepton {
 	E, MU, TAU,
---
+++ src/constant.cpp
@@ -0,0 +1,9 @@
+#include "sidis/constant.hpp"
+
+#include <limits>
+
+using namespace sidis;
+using namespace sidis::constant;
+
+Real const constant::INF = std::numeric_limits<Real>::infinity();
+
