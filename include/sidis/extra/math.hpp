d57b68801106e4cd25441c42df6789ba1fd16692
Duane Byer <duane.byer@gmail.com>
2020-12-01 12:03:14 -0500

    Create clamp utility function

--- include/sidis/extra/math.hpp
+++ include/sidis/extra/math.hpp
@@ -4,6 +4,35 @@
 namespace sidis {
 namespace math {
 
+inline float clamp(float min, float x, float max) {
+	if (!(x > min)) {
+		return min;
+	} else if (!(x < max)) {
+		return max;
+	} else {
+		return x;
+	}
+}
+
+inline double clamp(double min, double x, double max) {
+	if (!(x > min)) {
+		return min;
+	} else if (!(x < max)) {
+		return max;
+	} else {
+		return x;
+	}
+}
+inline long double clamp(long double min, long double x, long double max) {
+	if (!(x > min)) {
+		return min;
+	} else if (!(x < max)) {
+		return max;
+	} else {
+		return x;
+	}
+}
+
 inline float sq(float x) {
 	return x * x;
 }
