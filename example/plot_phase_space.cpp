9f66a5bca5bdffeb461e766fad60f9b2260d291b
Duane Byer <duane.byer@gmail.com>
2021-01-20 22:58:53 -0500

    Add example project for plotting phsae space contours

---
+++ example/plot_phase_space.cpp
@@ -0,0 +1,207 @@
+#include <cmath>
+#include <iostream>
+#include <limits>
+#include <memory>
+#include <random>
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+#include <TApplication.h>
+#include <TCanvas.h>
+#include <TH1.h>
+#include <TH1D.h>
+#include <TH2D.h>
+#include <TH3D.h>
+
+#include <sidis/sidis.hpp>
+#include <sidis/extra/bounds.hpp>
+
+using namespace sidis;
+using namespace sidis::constant;
+using namespace sidis::kin;
+using namespace sidis::math;
+
+// Plots the kinematically allowed phase space region. This program can generate
+// plots in 1, 2, or 3 kinematic variables.
+int main(int argc, char** argv) {
+	int argc_root = 1;
+
+	Real M_th = MASS_P + MASS_PI_0;
+	Lepton beam = Lepton::E;
+	Nucleus target = Nucleus::P;
+	Hadron hadron = Hadron::PI_P;
+
+	// Parse command line arguments.
+	Real beam_energy;
+	bool radiative;
+	std::vector<std::string> axes;
+	try {
+		if (argc < 4 || argc > 6) {
+			throw std::invalid_argument(
+				"Unexpected number of command line arguments");
+		}
+		beam_energy = std::stold(argv[1]);
+		std::string radiative_str = argv[2];
+		if (radiative_str == "true"
+				|| radiative_str == "on"
+				|| radiative_str == "rad") {
+			radiative = true;
+		} else if (radiative_str == "false"
+				|| radiative_str == "off"
+				|| radiative_str == "nrad") {
+			radiative = false;
+		} else {
+			throw std::out_of_range(
+				"Must select radiative (rad) or "
+				"non-radiative (nrad) phase space point");
+		}
+
+		for (std::size_t idx = 3; idx < argc; ++idx) {
+			std::string axis_var = argv[idx];
+			if (axis_var == "x"
+					|| axis_var == "y" || axis_var == "Q_sq"
+					|| axis_var == "z"
+					|| axis_var == "ph_t" || axis_var == "ph_t_sq"
+					|| axis_var == "phi_h"
+					|| axis_var == "phi"
+					|| (radiative && axis_var == "tau")
+					|| (radiative && axis_var == "phi_k")
+					|| (radiative && axis_var == "R")) {
+				axes.push_back(axis_var);
+			} else {
+				throw std::out_of_range(
+					"Unrecognized kinematic variable '" + axis_var + "'");
+			}
+		}
+	} catch (std::exception const& e) {
+		std::cerr << "Error: " << e.what() << std::endl;
+		std::cout << "Usage: "
+			<< "plot_phase_space <E_b> <rad,nrad> "
+			<< "<kin. var. x> [kin. var. y] [kin. var. z]"
+			<< std::endl;
+		return 1;
+	}
+
+	std::vector<std::vector<Real> > data;
+	std::vector<Real> data_mins;
+	std::vector<Real> data_maxs;
+	for (std::size_t idx = 0; idx < axes.size(); ++idx) {
+		data.push_back(std::vector<Real>());
+		data_maxs.push_back(std::numeric_limits<Real>::lowest());
+		data_mins.push_back(std::numeric_limits<Real>::max());
+	}
+
+	// Generate events randomly in phase space.
+	Initial initial_state(target, beam, beam_energy);
+	std::random_device rd;
+	std::mt19937 rng(rd());
+	std::uniform_real_distribution<Real> dist(0., 1.);
+	std::cout << "Generating points in phase space." << std::endl;
+	std::size_t num_points = 0;
+	if (axes.size() == 1) {
+		num_points = 100000;
+	} else if (axes.size() == 2) {
+		num_points = 1000000;
+	} else if (axes.size() == 3) {
+		num_points = 10000000;
+	}
+	for (std::size_t n = 0; n < num_points; ++n) {
+		Kinematics kin;
+		KinematicsRad kin_rad;
+		do {
+			Real x = x_bounds(initial_state).lerp(dist(rng));
+			Real y = y_bounds(initial_state, x).lerp(dist(rng));
+			Real z = z_bounds(initial_state, hadron, M_th, x, y).lerp(dist(rng));
+			Real ph_t_sq = ph_t_sq_bounds(initial_state, hadron, M_th, x, y, z).lerp(dist(rng));
+			Real phi_h = Bounds(-PI, PI).lerp(dist(rng));
+			Real phi = Bounds(-PI, PI).lerp(dist(rng));
+			PhaseSpace phase_space { x, y, z, ph_t_sq, phi_h, phi };
+			kin = Kinematics(initial_state, phase_space, hadron, M_th);
+
+			Real tau = tau_bounds(kin).lerp(dist(rng));
+			Real phi_k = Bounds(-PI, PI).lerp(dist(rng));
+			Real R = R_bounds(kin, tau, phi_k).lerp(dist(rng));
+			kin_rad = KinematicsRad(kin, tau, phi_k, R);
+		} while ((!radiative && !valid(kin)) || (radiative && !valid(kin_rad)));
+
+		for (std::size_t idx = 0; idx < axes.size(); ++idx) {
+			std::string axis_var = axes[idx];
+			Real value = 0.;
+			if (axis_var == "x") {
+				value = kin.x;
+			} else if (axis_var == "y") {
+				value = kin.y;
+			} else if (axis_var == "Q_sq") {
+				value = kin.Q_sq;
+			} else if (axis_var == "z") {
+				value = kin.z;
+			} else if (axis_var == "ph_t") {
+				value = kin.ph_t;
+			} else if (axis_var == "ph_t_sq") {
+				value = kin.ph_t_sq;
+			} else if (axis_var == "phi_h") {
+				value = kin.phi_h;
+			} else if (axis_var == "phi") {
+				value = kin.phi;
+			} else if (axis_var == "tau") {
+				value = kin_rad.tau;
+			} else if (axis_var == "phi_k") {
+				value = kin_rad.phi_k;
+			} else if (axis_var == "R") {
+				value = kin_rad.R;
+			}
+			if (value < data_mins[idx]) {
+				data_mins[idx] = value;
+			}
+			if (value > data_maxs[idx]) {
+				data_maxs[idx] = value;
+			}
+			data[idx].push_back(value);
+		}
+	}
+
+	// Plot the events.
+	std::cout << "Plotting." << std::endl;
+	TApplication app("Phase space plots", &argc_root, argv);
+	TCanvas canvas("Phase space canvas");
+	std::unique_ptr<TH1> hist;
+	if (axes.size() == 1) {
+		TH1D* h = new TH1D(
+			"Phase space", "Phase space",
+			100, data_mins[0], data_maxs[0]);
+		h->FillN(data[0].size(), data[0].data(), nullptr);
+		h->GetXaxis()->SetTitle(axes[0].c_str());
+		h->Draw("hist");
+		hist.reset(h);
+	} else if (axes.size() == 2) {
+		TH2D* h =new TH2D(
+			"Phase space", "Phase space",
+			100, data_mins[0], data_maxs[0],
+			100, data_mins[1], data_maxs[1]);
+		h->FillN(data[0].size(), data[0].data(), data[1].data(), nullptr);
+		h->GetXaxis()->SetTitle(axes[0].c_str());
+		h->GetYaxis()->SetTitle(axes[1].c_str());
+		h->Draw("colz");
+		hist.reset(h);
+	} else if (axes.size() == 3) {
+		TH3D* h = new TH3D(
+			"Phase space", "Phase space",
+			100, data_mins[0], data_maxs[0],
+			100, data_mins[1], data_maxs[1],
+			100, data_mins[2], data_maxs[2]);
+		for (std::size_t idx = 0; idx < data[0].size(); ++idx) {
+			h->Fill(data[0][idx], data[1][idx], data[2][idx]);
+		}
+		h->GetXaxis()->SetTitle(axes[0].c_str());
+		h->GetYaxis()->SetTitle(axes[1].c_str());
+		h->GetZaxis()->SetTitle(axes[2].c_str());
+		h->Draw("box");
+		hist.reset(h);
+	}
+
+	app.Run();
+
+	return 0;
+}
+
