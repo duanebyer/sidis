347e066ad9d9983b72e0b7d039c58aec548e7bc0
Duane Byer <duane.byer@gmail.com>
2021-02-05 16:04:23 -0500

    Add more features to Bounds type.

--- include/sidis/extra/bounds.hpp
+++ include/sidis/extra/bounds.hpp
@@ -8,27 +8,84 @@ namespace math {
 
 struct Bounds {
 	static Bounds const INVALID;
+	static Bounds const ZERO;
+	static Bounds const UNIT;
 	static Bounds const FULL;
+	static Bounds const POSITIVE;
+	static Bounds const NEGATIVE;
 
-	Real min;
-	Real max;
-	Bounds(Real min, Real max) : min(min), max(max) { }
-	bool operator()(Real x) const {
-		return min <= x && x < max;
+private:
+	Real _min;
+	Real _max;
+
+public:
+
+	Real min() const {
+		return _min;
 	}
-	Real valid() const {
-		return min < max;
+	Real max() const {
+		return _max;
+	}
+
+	Bounds(Real min, Real max);
+	bool contains(Real x) const {
+		return _min <= x && x < _max;
 	}
 	Real lerp(Real x) const {
-		return (1. - x) * min + x * max;
+		return (1. - x) * _min + x * _max;
+	}
+	Real valid() const {
+		return _min <= _max;
 	}
 	Real size() const {
-		return max - min;
+		return _max - _min;
 	}
+
+	// Combining bounds.
+	Bounds& operator&=(Bounds const& rhs);
+	Bounds& operator|=(Bounds const& rhs);
+
+	// Arithmetic operations.
+	Bounds& operator+=(Real s);
+	Bounds& operator-=(Real s);
+	Bounds& operator*=(Real s);
+	Bounds& operator/=(Real s);
 };
 
-Bounds operator&(Bounds lhs, Bounds rhs);
-Bounds operator|(Bounds lhs, Bounds rhs);
+inline Bounds operator&(Bounds lhs, Bounds const& rhs) {
+	lhs &= rhs;
+	return lhs;
+}
+inline Bounds operator|(Bounds lhs, Bounds const& rhs) {
+	lhs |= rhs;
+	return lhs;
+}
+
+// Arithmetic operations.
+inline Bounds operator+(Bounds lhs, Real rhs) {
+	lhs += rhs;
+	return lhs;
+}
+inline Bounds operator+(Real lhs, Bounds rhs) {
+	rhs += lhs;
+	return rhs;
+}
+inline Bounds operator-(Bounds lhs, Real rhs) {
+	lhs -= rhs;
+	return lhs;
+}
+inline Bounds operator*(Bounds lhs, Real rhs) {
+	lhs *= rhs;
+	return lhs;
+}
+inline Bounds operator*(Real lhs, Bounds rhs) {
+	rhs *= lhs;
+	return rhs;
+}
+inline Bounds operator/(Bounds lhs, Real rhs) {
+	lhs /= rhs;
+	return lhs;
+}
 
 }
 }
--- src/bounds.cpp
+++ src/bounds.cpp
@@ -9,19 +9,74 @@ using namespace sidis::math;
 Bounds const Bounds::INVALID = Bounds(
 	std::numeric_limits<Real>::quiet_NaN(),
 	std::numeric_limits<Real>::quiet_NaN());
+Bounds const Bounds::ZERO = Bounds(0., 0.);
+Bounds const Bounds::UNIT = Bounds(0., 1.);
 Bounds const Bounds::FULL = Bounds(
 	std::numeric_limits<Real>::lowest(),
 	std::numeric_limits<Real>::max());
+Bounds const Bounds::POSITIVE = Bounds(
+	0.,
+	std::numeric_limits<Real>::max());
+Bounds const Bounds::NEGATIVE = Bounds(
+	std::numeric_limits<Real>::lowest(),
+	0.);
 
-Bounds math::operator&(Bounds lhs, Bounds rhs) {
-	return Bounds(
-		std::fmax(lhs.min, rhs.min),
-		std::fmin(lhs.max, rhs.max));
+Bounds::Bounds(Real min, Real max) : _min(min), _max(max) {
+	if (!(_min <= _max)) {
+		*this = Bounds::INVALID;
+	}
 }
 
-Bounds math::operator|(Bounds lhs, Bounds rhs) {
-	return Bounds(
-		std::fmin(lhs.min, rhs.min),
-		std::fmax(lhs.max, rhs.max));
+Bounds& Bounds::operator&=(Bounds const& rhs) {
+	if (std::isnan(_min) || std::isnan(rhs._min)
+			|| std::isnan(_max) || std::isnan(rhs._max)) {
+		*this = Bounds::INVALID;
+	} else {
+		_min = std::fmax(_min, rhs._min);
+		_max = std::fmin(_max, rhs._max);
+		if (!(_min <= _max)) {
+			*this = Bounds::INVALID;
+		}
+	}
+	return *this;
+}
+Bounds& Bounds::operator|=(Bounds const& rhs) {
+	if (std::isnan(_min) || std::isnan(rhs._min)
+			|| std::isnan(_max) || std::isnan(rhs._max)) {
+		*this = Bounds::INVALID;
+	} else {
+		_min = std::fmin(_min, rhs._min);
+		_max = std::fmax(_max, rhs._max);
+	}
+	return *this;
+}
+
+Bounds& Bounds::operator+=(Real s) {
+	_min += s;
+	_max += s;
+	return *this;
+}
+Bounds& Bounds::operator-=(Real s) {
+	_min -= s;
+	_max -= s;
+	return *this;
+}
+Bounds& Bounds::operator*=(Real s) {
+	if (s < 0.) {
+		*this = Bounds::INVALID;
+	} else {
+		_min *= s;
+		_max *= s;
+	}
+	return *this;
+}
+Bounds& Bounds::operator/=(Real s) {
+	if (s <= 0.) {
+		*this = Bounds::INVALID;
+	} else {
+		_min /= s;
+		_max /= s;
+	}
+	return *this;
 }
 
