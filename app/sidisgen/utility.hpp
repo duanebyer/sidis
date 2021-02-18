bc7e33a119921a6a8f61cfc055a7fe113a3e0793
Duane Byer <duane.byer@gmail.com>
2021-02-18 09:55:00 -0500

    Remove unused headers

--- app/sidisgen/utility.hpp
+++ app/sidisgen/utility.hpp
@@ -2,9 +2,6 @@
 #define SIDISGEN_UTILITY_HPP
 
 #include <ostream>
-#include <type_traits>
-
-#include <iostream>
 
 // Draws a progress bar in the terminal.
 bool write_progress_bar(std::ostream& os, unsigned percent, unsigned width=70);
