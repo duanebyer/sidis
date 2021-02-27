f1f1d14888019f5471e815847fee2a3bc3c42d80
Duane Byer <duane.byer@gmail.com>
2021-02-27 15:29:59 -0500

    Document constant.hpp

--- include/sidis/constant.hpp
+++ include/sidis/constant.hpp
@@ -5,12 +5,24 @@
 
 namespace sidis {
 
+/**
+ * \defgroup Constant Constants
+ * \addtogroup Constant
+ */
+/// \{
+
+/// Mathematical constant \f$\pi\f$.
 static Real const PI = 3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679821480865132823066470938446L;
+/// Mathematical constant \f$e\f$.
 static Real const E = 2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174136L;
+/// Positive infinity.
 extern Real const INF;
 
+/// Fine structure constant.
 static Real const ALPHA = 7.2973525664e-3L;
 
+/// \name Particle masses
+/// \{
 static Real const MASS_E = 0.0005109989461L;
 static Real const MASS_MU = 0.1056583745L;
 static Real const MASS_TAU = 1.77686L;
@@ -25,6 +37,9 @@ static Real const MASS_K_0 = 0.497611L;
 static Real const MASS_K = 0.493677L;
 static Real const MASS_PHI = 1.019461L;
 static Real const MASS_J_PSI = 3.096900L;
+/// \}
+
+/// \}
 
 }
 
