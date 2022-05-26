87c41dbb09120c718e1ba19d6b26a1db056ce9f8
Duane Byer <duane.byer@gmail.com>
2022-05-26 18:16:29 -0400

    Move check until after the output for the type of FOAM being loaded

--- app/sidisgen/main.cpp
+++ app/sidisgen/main.cpp
@@ -588,10 +588,10 @@ int command_generate(char const* params_file_name) {
 	Params foam_params;
 	foam_params.read_root(foam_file);
 	for (EventType gen_type : gen_types) {
-		params.compatible_with_foam(gen_type, foam_params);
 		char const* gen_name = event_type_name(gen_type);
 		char const* gen_key = event_type_short_name(gen_type);
 		std::cout << "Loading " << gen_name << " FOAM from file." << std::endl;
+		params.compatible_with_foam(gen_type, foam_params);
 		try {
 			std::string* data = foam_file.Get<std::string>(gen_key);
 			if (data == nullptr) {
